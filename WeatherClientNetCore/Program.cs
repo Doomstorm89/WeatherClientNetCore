3bc0beec6778741cf49a9a1b64557d7c8eac6042
doomstorm1989@gmail.com <doomstorm1989@gmail.com>
2021-04-26 18:37:09 +0800

    Добавьте файлы проекта.

---
+++ WeatherClientNetCore/Program.cs
@@ -0,0 +1,25 @@
+﻿using Grpc.Net.Client;
+using System;
+using WeatherGrpcService;
+
+namespace WeatherClientNetCore
+{
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            string name;
+            do
+            {
+                using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+                var client = new Weather.WeatherClient(channel);
+                Console.Write("Введите город или stop для выхода: ");
+                name = Console.ReadLine();
+                var reply = await client.GetWeatherAsync(new WeatherRequest { Name = name });
+                Console.WriteLine("Ответ сервера: " + reply.Message);
+
+            } while (name!="stop");
+            
+        }
+    }
+}
